1e6122c78711c59ddd0c25eaeb5df15702e1da2a
Makoto Tanabe <xekid78@outlook.com>
2018-03-29 11:18:33 +0900

    Add files via upload

---
+++ Foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Foreach
+    {
+        static void Main(string[] args)
+        {
+            string[] team = {"佐藤", "鈴木", "田中", "岸田", "有森"};
+
+            foreach (var name in team)
+            {
+                Console.WriteLine(name + "さん");
+            }
+        }
+    }
+}
